f61097382b0dbdb92e6b5dded93463e6801ae8d6
waterada <w.terada@gmail.com>
2015-01-31 15:48:31 +0900

    初回追加

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# doorkeeper-chrome-ex
+doorkeeper-chrome-ex
+=====================
+
+- Doorkeeper 用の自己紹介で使う Chrome 拡張です。PHP勉強会で使っているものです。
+- Doorkeeper のイベントページを表示するとページ下部にアイコンを拡大させたリストが作成されます。
+
+
+拡張機能の更新手順(開発側)
+---------------------------
+
+1. `src/manifest.json` の `version` をインクリメントする(たとえば `1.4`)。
+2. `src/` の中を更新する。
+3. 拡張機能( `chrome://extensions` )の画面で『デベロッパーモード』をONにする。
+4. 対象の拡張機能の `有効にする` を OFF にする。
+5. 拡張機能の `パッケージ化されていない拡張機能を読み込む...` ボタンで `src` のディレクトリを選択する。
+6. これで拡張機能が読み込まれるので画面でテストする。ソースを修正したら `リロード (Ctrl + R)` のリンクをクリックすることで、拡張機能が最新に更新される。
+7. 問題なければ commit して github に push。
+8. `src` ディレクトリを圧縮して `src.zip` を作る。
+9. 拡張機能の画面で、開発用の拡張機能は削除して、元の拡張機能の `有効にする` をONにする。
+10. 拡張機能の画面で `デベロッパーモード` をOFFにする。
+11. google chrome の デベロッパー・ダッシュボード(https://chrome.google.com/webstore/developer/dashboard)を表示。
+12. 「編集する」をクリックし、「更新パッケージをアップロード」ボタンでアップロード画面に進み、 `src.zip` ファイルをアップロードする。
+13. 「変更を公開」ボタンを押す。
+14. Chrome を再起動してバージョンがあがったことを確認する。
+
+
+インストール方法
+-----------------
+
+1. Google Chrome を開く。
+2. https://chrome.google.com/webstore/detail/redmine-extension-for-gmo/cncopmaajfkdihcmkalbffgpmbaiaejl
+にアクセスして追加する。
+
+
+機能
+-----
+
+
+```
---
+++ src/content_script.js
@@ -0,0 +1,57 @@
+﻿//フッターの下に人物リストを作る
+var $introduction = $('<div></div>').css({
+    "lineHeight": "normal"
+}).appendTo('body');
+
+//反映ボタン
+var $button = $('<input type="button" value="自己紹介構築">').appendTo('body');
+$button.click(function(){
+    $introduction.html("terada");
+    $('.user-profile-details').each(function() {
+        var $profile1 = $(this);
+        var $profile2 = $('<div></div>').css({
+            "margin": "40px"
+        });
+        //名前
+        var $name = $('<div></div>').css({
+            "fontSize": "40px"
+        }).text($profile1.children('.user-name').text());
+        $name.appendTo($profile2);
+        //アバター
+        var $img = $profile1.children('.user-avatar').clone();
+        var src = $img.attr('src');
+        //console.log("---");
+        //console.log(src);
+        if (src.match(/gravatar/)) {
+            //console.log("match");
+            src = src.replace(/s=25/, "s=160");
+            //console.log(src);
+            $img.attr("src", src);
+            $img.attr("data-src", src);
+        }
+        $img.attr('height', '160');
+        $img.attr('width', '160');
+        var $avatar = $('<div></div>');
+        $avatar.append($img);
+        $avatar.appendTo($profile2);
+        //ソーシャル
+        var $socials1 = $profile1.find('.external-profile-link');
+        var $socials2 = $('<div></div>');
+        $socials1.each(function() {
+            var $social = $('<div></div>').css({
+                "marginTop": "5px"
+            });
+            var $link = $(this).clone();
+            $link.appendTo($social);
+            $link.find("i").css({
+                "fontSize": "30px"
+            }).text(" " + $link.attr("data-original-title"));
+            $social.appendTo($socials2);
+        });
+        $socials2.appendTo($profile2);
+
+        //追加
+        $profile2.appendTo($introduction);
+        $('<hr>').appendTo($introduction);
+    });
+});
